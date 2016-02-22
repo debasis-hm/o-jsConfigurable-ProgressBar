ee9e1d843e06f257f7075ebce3da0842d235c152
Satapathy, Debasis <Debasis.Satapathy@pearson.com>
2016-02-22 12:53:40 +0530

    Code and configuration changes

---
+++ src/js/jsConfiurableProgressbar.js
@@ -0,0 +1,66 @@
+function origamijsConfigurableDistro_bar(parms) {
+
+		let db = this;
+
+	    db.draw_DistroBar = function(parms){
+
+
+   				if( parms.animationstyle == "o-progressBar"){
+
+					let jsonDatalength = parms.dataLabels.length;
+
+					if(jsonDatalength){
+
+						var index = -1;
+
+						setInterval(function() {
+							index++;
+							if(index < jsonDatalength){
+
+								var parentDiv = document.getElementById(parms.dataLabels[index].progresshtmlElement);
+								var outerclsinfo = parms.dataLabels[index].outerBackgroundclassName;
+								if(outerclsinfo){parentDiv.classList.add(outerclsinfo);}							
+
+										var innerDiv = document.createElement("DIV");
+										var innerclsinfo = parms.dataLabels[index].className;
+										innerDiv.classList.add(innerclsinfo);
+										parentDiv.appendChild(innerDiv);
+										var width = 0;
+										var progressjson = parms.dataLabels[index].progress;
+										if(progressjson){
+
+											var progress = setInterval(function () { 
+													if (width == progressjson) {
+														 clearInterval(progress);						
+													}
+													else{
+														width++;
+														innerDiv.style.display = "inline-block";
+														innerDiv.style.width = width+ "%";													   
+													    innerDiv.innerHTML = width+ "%";													    	
+													}
+																	    
+										     }, 30); 
+										}
+
+
+							}
+						},100);
+					}
+				}
+				else {
+
+     				console.log("please provide a type .");
+				}
+
+	    };
+
+	};
+
+ let ojsConfigurableDistrobarobj = new origamijsConfigurableDistro_bar();
+	export default function (parms) {
+		if(parms!= undefined){
+			   ojsConfigurableDistrobarobj.draw_DistroBar(parms);
+		}	   
+	 
+	}
